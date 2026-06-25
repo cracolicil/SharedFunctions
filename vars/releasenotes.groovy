a5e013f6bab92f78a743de17a51be271961be70e
Leonardo Cracolici | Fulcri <leonardocracolici@fulcri.it>
2026-06-25 17:41:38 +0200

    corretto errore

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -5,13 +5,14 @@ import groovy.io.*;
 def call(Map config=[:]){
     def dir = new File(pwd());
 
-    new File(dir.path + '\\releasenotes.txt').withWriter('utf-8')
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8'){ writer ->
 
-    dir.eachFileRecurse(FileType.ANY) { file ->
-        if(file.isDirectory()){
-            writer.writeLine(file.name);
-        }else{
-            writer.writeLine(file.name + '\t' + file.length());
+        dir.eachFileRecurse(FileType.ANY) { file ->
+            if(file.isDirectory()){
+                writer.writeLine(file.name);
+            }else{
+                writer.writeLine(file.name + '\t' + file.length());
+            }
         }
     }
 }
