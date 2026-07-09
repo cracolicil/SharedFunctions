51332140015309867c924540d29a41f90bec7c09
Leonardo Cracolici | Fulcri <leonardocracolici@fulcri.it>
2026-07-09 10:31:51 +0200

    test script

---
+++ vars/soapuiTestRun.groovy
@@ -0,0 +1,23 @@
+def call(Map cfg = [:]) {
+    if(!cfg.endPoint) {
+        error "parametro obbligatorio mancante: endPoint"
+    }
+    if(!cfg.testSuite) {
+        error "parametro obbligatorio mancante: testSuite"
+    }
+    if(!cfg.propertiesFile) {
+        error "parametro obbligatorio mancante: propertiesFile"
+    }
+    if(!cfg.projectFile) {
+        error "parametro obbligatorio mancante: projectFile"
+    }
+
+    bat '''
+        "%TESTRUNNER_PATH%\\testrunner.bat" ^
+        -e${#TestSuite#${cfg.endPoint}} ^
+        -r -J -s${cfg.testSuite} -S ^
+        -PprojectPropertiesPath="%WORKSPACE%\\${cfg.propertiesFile}" ^
+        -PprojectPathTestRunner="%WORKSPACE%" ^
+        "%WORKSPACE%\\${cfg.projectFile}"
+    '''
+}
